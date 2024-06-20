85f01cad83b5ce936e83755884950262e5a4634d
Ilya Avramchuk <163721512+randredNSTU@users.noreply.github.com>
2024-06-20 23:47:05 +0700

    Add files via upload

--- Ex1.2.cpp
+++ Ex1.2.cpp
@@ -1,28 +1,56 @@
 #include <iostream>
-#include <vector>
+#include <iomanip>
 
-void printMatrix(int N) {
-    std::vector<std::vector<int>> matrix(N, std::vector<int>(N, 100));
-
+void initializeMatrix(int** matrix, int N) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             if (i == j) {
-                std::cout << "000 ";
+                matrix[i][j] = 0;
+            } else if (j == 0 || j == N - 1) {
+                matrix[i][j] = 100;
+            } else if (i == 0) {
+                matrix[i][j] = 100 + N - 1 - j;
+            } else if (i == N - 1) {
+                matrix[i][j] = 100 + j;
+            } else if (i < j) {
+                matrix[i][j] = 100 + j - i;
             } else {
-                matrix[i][j] += (i > j) ? i - j : j - i;
-                std::cout << matrix[i][j] << " ";
+                matrix[i][j] = 100 + i - j;
             }
         }
-        std::cout << "\n";
+    }
+}
+
+void printMatrix(int** matrix, int N) {
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (matrix[i][j] == 0) {
+                std::cout << "000 ";
+            } else {
+                std::cout << std::setw(3) << std::setfill('0') << matrix[i][j] << " ";
+            }
+        }
+        std::cout << std::endl;
     }
 }
 
 int main() {
     int N;
-    std::cout << "Введите размерность матрицы N: ";
+    std::cout << "Enter the size of the matrix: ";
     std::cin >> N;
 
-    printMatrix(N);
+    int** matrix = new int*[N];
+    for (int i = 0; i < N; ++i) {
+        matrix[i] = new int[N];
+    }
+
+    initializeMatrix(matrix, N);
+    printMatrix(matrix, N);
+
+    for (int i = 0; i < N; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
 
     return 0;
 }
